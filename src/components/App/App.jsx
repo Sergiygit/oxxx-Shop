42a2dec5bf7cea377386742e22495e2ab266d2f0
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-10-24 19:12:46 +0300

    setting up the project, the beginning
    add yarn,  and dependencies
    
      "sass": "^1.69.4",
      "swiper": "^11.0.1",
      "react-redux": "^8.1.3",
      "react-router-dom": "^6.17.0",
      "framer-motion": "^10.16.4",
      "react-animate-on-scroll": "^2.1.7",
      "contentful": "^10.6.4",
      "framer-motion": "^10.16.4",

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import AppRoutes from './AppRoutes'
+
+import '../../styles/index.scss'
+
+const App = () => (
+	<div className='app'>
+		<Header />
+		<AppRoutes />
+		<Footer />
+	</div>
+)
+
+export default App
