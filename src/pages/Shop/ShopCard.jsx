90e1448cc25f6a495168ae6edd0366c83d5e6e5f
Nazar Lyubchinsky <nazar.muxal@gmail.com>
2025-07-03 12:37:12 +0300

    add

--- src/pages/Shop/ShopCard.jsx
+++ src/pages/Shop/ShopCard.jsx
@@ -46,7 +46,7 @@ const ShopCard = memo(({ el }) => {
 		<li className="shop-list__item" key={id}>
 			{!discount ? <span className="shop-list__item-price">{price} $</span> : <span className="shop-list__item-priceSale">{calculateDiscountedPrice(price, discount)} $</span>}
 			<Link to={`/shop/product/${id}`} >
-				<LazyLoadImage effect='blur' className="shop-list__item-image" src={url} alt={el.title} />
+				<LazyLoadImage effect='blur' className="shop-list__item-image" src={url } alt={el.title} />
 			</Link>
 			<h1 className="shop-list__item-title">{title}</h1>
 			{/* <hr /> */}
@@ -57,3 +57,37 @@ const ShopCard = memo(({ el }) => {
 })
 
 export default ShopCard
+
+
+
+// import React, { memo } from 'react'
+// import { Link } from 'react-router-dom';
+
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import 'react-lazy-load-image-component/src/effects/blur.css';
+
+// import { calculateDiscountedPrice } from '../../utils/common';
+
+// const ShopCard = memo(({ el }) => {
+// // 	const { sys: { id }, title, cover: { url }, description, price, discount } = el;
+
+// 	const { sys: { id }, title, cover,  price, discount } = el;
+
+// const imageUrl = cover?.url || '/no-image.jpg';
+// const url = cover?.url
+// console.log(url)
+// 	return (
+// 		<li className="shop-list__item" key={id}>
+// 			{!discount ? <span className="shop-list__item-price">{price} $</span> : <span className="shop-list__item-priceSale">{calculateDiscountedPrice(price, discount)} $</span>}
+// 			<Link to={`/shop/product/${id}`} >
+// 				<LazyLoadImage effect='blur' className="shop-list__item-image" src={imageUrl} alt={el.title} />
+// 			</Link>
+// 			<h1 className="shop-list__item-title">{title}</h1>
+// 			{/* <hr /> */}
+// 			{/* <p className="shop-list__item-description">{description} </p> */}
+// 			{/* <Link to={`/shop/${id}`} className="shop-list__item-btn btn">Buy Now</Link> */}
+// 		</li>
+// 	)
+// })
+
+// export default ShopCard
--- src/pages/Shop/ShopPage.jsx
+++ src/pages/Shop/ShopPage.jsx
@@ -1,45 +1,86 @@
+// import React, { useState } from 'react'
+// import Preloader from '../../components/Preloader/Preloader'
+// import Section from '../../components/Section/Section'
+// import PageTitle from '../../components/Title/PageTitle'
+// import { useShopItems } from '../../hooks/useShopitems'
+
+// //components 
+// import ShopCard from './ShopCard'
+// import ShopCategoryList from './ShopCategoryList'
+
+// const ShopPage = () => {
+
+// 	const { items = [], isLoading } = useShopItems()
+
+// 	const [filtered, setFiltered] = useState([]);
+
+	
+
+// 	return (
+// 		<Section className="shop-section page">
+// 			<div className="container">
+// 				<PageTitle text='Shop' />
+
+// 				{isLoading ?
+// 					<Preloader /> : (
+// 						<>
+// 							<ShopCategoryList filtered={filtered} setFiltered={setFiltered} />
+// 							<ul className='shop-list'>
+// 								{filtered.length ?
+// 									filtered.map((el, idx) => (
+// 										<ShopCard key={idx} el={el} />
+// 									)) :
+// 									items.map((el, idx) => (
+// 										<ShopCard key={idx} el={el} />
+// 									))
+// 								}
+// 							</ul>
+// 						</>
+// 					)}
+// 			</div>
+// 		</Section>
+// 	)
+// }
+
+// export default ShopPage
 import React, { useState } from 'react'
 import Preloader from '../../components/Preloader/Preloader'
 import Section from '../../components/Section/Section'
 import PageTitle from '../../components/Title/PageTitle'
 import { useShopItems } from '../../hooks/useShopitems'
 
-//components 
+// components
 import ShopCard from './ShopCard'
 import ShopCategoryList from './ShopCategoryList'
 
 const ShopPage = () => {
+  const { items = [], isLoading } = useShopItems()
+  const [filtered, setFiltered] = useState([])
 
-	const { items = [], isLoading } = useShopItems()
+  return (
+    <Section className="shop-section page">
+      <div className="container">
+        
+        <PageTitle text="Shop" />
 
-	const [filtered, setFiltered] = useState([]);
-
-	return (
-		<Section className="shop-section page">
-			<div className="container">
-				<PageTitle text='Shop' />
-
-				{isLoading ?
-					<Preloader /> : (
-						<>
-							<ShopCategoryList filtered={filtered} setFiltered={setFiltered} />
-							<ul className='shop-list'>
-								{filtered.length ?
-									filtered.map((el, idx) => (
-										<ShopCard key={idx} el={el} />
-									)) :
-									items.map((el, idx) => (
-										<ShopCard key={idx} el={el} />
-									))
-								}
-							</ul>
-						</>
-					)}
-			</div>
-		</Section>
-	)
+        {isLoading ? (
+          <Preloader />
+        ) : (
+          <>
+            <ShopCategoryList filtered={filtered} setFiltered={setFiltered} />
+            <ul className="shop-list">
+              {(filtered.length ? filtered : items).map((el, idx) => (
+                <ShopCard key={idx} el={el} />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </Section>
+  )
 }
 
 export default ShopPage
+
 
 
