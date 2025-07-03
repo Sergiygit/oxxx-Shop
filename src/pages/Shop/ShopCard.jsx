09518ffc86f16f5c10b9098c5c0d64bd3fb0208d
Nazar Lyubchinsky <nazar.muxal@gmail.com>
2025-07-03 15:57:50 +0300

    ad

--- src/pages/Shop/ShopCard.jsx
+++ src/pages/Shop/ShopCard.jsx
@@ -1,4 +1,36 @@
 
+// import React, { memo } from 'react'
+// import { Link } from 'react-router-dom';
+
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import 'react-lazy-load-image-component/src/effects/blur.css';
+
+// import { calculateDiscountedPrice } from '../../utils/common';
+
+// const ShopCard = memo(({ el }) => {
+// // 	const { sys: { id }, title, cover: { url }, description, price, discount } = el;
+
+// 	const { sys: { id }, title, cover: { url },  price, discount } = el;
+
+
+// 	return (
+// 		<li className="shop-list__item" key={id}>
+// 			{!discount ? <span className="shop-list__item-price">{price} $</span> : <span className="shop-list__item-priceSale">{calculateDiscountedPrice(price, discount)} $</span>}
+// 			<Link to={`/shop/product/${id}`} >
+// 				<LazyLoadImage effect='blur' className="shop-list__item-image" src={url } alt={el.title} />
+// 			</Link>
+// 			<h1 className="shop-list__item-title">{title}</h1>
+// 			{/* <hr /> */}
+// 			{/* <p className="shop-list__item-description">{description} </p> */}
+// 			{/* <Link to={`/shop/${id}`} className="shop-list__item-btn btn">Buy Now</Link> */}
+// 		</li>
+// 	)
+// })
+
+// export default ShopCard
+
+
+
 import React, { memo } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -7,25 +39,59 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import { calculateDiscountedPrice } from '../../utils/common';
 
+// Якщо логотип у /public, можна так:
+// const watermarkUrl = '/public/images/0-02-05-2ea2b8646444959a9191c5feec65ae5a543694a138425375e851378babbf180d_f52cd4cd5a29a098.jpg'
+
+// Або імпорт (якщо логотип у src/assets)
+// import watermarkUrl from '../../assets/watermark.png'
+
 const ShopCard = memo(({ el }) => {
-// 	const { sys: { id }, title, cover: { url }, description, price, discount } = el;
+  const { sys: { id }, title, cover: { url }, price, discount } = el;
 
-	const { sys: { id }, title, cover: { url },  price, discount } = el;
+  return (
+    <li className="shop-list__item" key={id}>
+      {!discount ? (
+        <span className="shop-list__item-price">{price} $</span>
+      ) : (
+        <span className="shop-list__item-priceSale">{calculateDiscountedPrice(price, discount)} $</span>
+      )}
 
+      <Link to={`/shop/product/${id}`}>
+        <div
+          style={{
+            position: 'relative',
+            display: 'inline-block',
+          }}
+        >
+          <LazyLoadImage
+            effect="blur"
+            className="shop-list__item-image"
+            src={url}
+            alt={title}
+          />
 
-	return (
-		<li className="shop-list__item" key={id}>
-			{!discount ? <span className="shop-list__item-price">{price} $</span> : <span className="shop-list__item-priceSale">{calculateDiscountedPrice(price, discount)} $</span>}
-			<Link to={`/shop/product/${id}`} >
-				<LazyLoadImage effect='blur' className="shop-list__item-image" src={url } alt={el.title} />
-			</Link>
-			<h1 className="shop-list__item-title">{title}</h1>
-			{/* <hr /> */}
-			{/* <p className="shop-list__item-description">{description} </p> */}
-			{/* <Link to={`/shop/${id}`} className="shop-list__item-btn btn">Buy Now</Link> */}
-		</li>
-	)
+          {/* <img
+            src='/images/0-02-05-2ea2b8646444959a9191c5feec65ae5a543694a138425375e851378babbf180d_f52cd4cd5a29a098.jpg'
+            alt="Watermark"
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%) rotate(-30deg)',
+              opacity: 0.2,
+              pointerEvents: 'none',
+              userSelect: 'none',
+              maxWidth: '70%', // або вкажи розмір логотипа
+              maxHeight: '70%',
+              zIndex: 10,
+            }}
+          /> */}
+        </div>
+      </Link>
+
+      <h1 className="shop-list__item-title">{title}</h1>
+    </li>
+  )
 })
 
 export default ShopCard
-
--- src/pages/Shop/ShopPage.jsx
+++ src/pages/Shop/ShopPage.jsx
@@ -1,43 +1,4 @@
 
-// import React, { useState } from 'react'
-// import Preloader from '../../components/Preloader/Preloader'
-// import Section from '../../components/Section/Section'
-// import PageTitle from '../../components/Title/PageTitle'
-// import { useShopItems } from '../../hooks/useShopitems'
-
-// // components
-// import ShopCard from './ShopCard'
-// import ShopCategoryList from './ShopCategoryList'
-
-// const ShopPage = () => {
-//   const { items = [], isLoading } = useShopItems()
-//   const [filtered, setFiltered] = useState([])
-
-//   return (
-//     <Section className="shop-section page">
-//       <div className="container">
-        
-//         <PageTitle text="Shop" />
-
-//         {isLoading ? (
-//           <Preloader />
-//         ) : (
-//           <>
-//             <ShopCategoryList filtered={filtered} setFiltered={setFiltered} />
-//             <ul className="shop-list">
-//               {(filtered.length ? filtered : items).map((el, idx) => (
-//                 <ShopCard key={idx} el={el} />
-//               ))}
-//             </ul>
-//           </>
-//         )}
-//       </div>
-//     </Section>
-//   )
-// }
-
-// export default ShopPage
-
 import React, { useState, useMemo, useEffect } from 'react'
 import { useSearchParams, useParams,  } from 'react-router-dom'
 import Preloader from '../../components/Preloader/Preloader'
